ed062ab176f920e7aebe493a5b84c404794f29cb
Andrew Palmer <atp@sdf.org>
2019-09-07 23:44:38 -0500

    Update scripts for 2019

--- scripts/generate_json.py
+++ scripts/generate_json.py
@@ -3,9 +3,9 @@ import re
 from datetime import date
 
 
-INPUT = '../static/wolfe-scores/2018/current.txt'
-GAMES_PATH = '../static/wolfe-scores/2018/games-db.json'
-TEAMS_PATH = '../static/wolfe-scores/2018/teams-db.json'
+INPUT = '../static/wolfe-scores/2019/current.txt'
+GAMES_PATH = '../static/wolfe-scores/2019/games-db.json'
+TEAMS_PATH = '../static/wolfe-scores/2019/teams-db.json'
 ROW_REGEX = re.compile(r'^(.{9}) (.{27}) (.{2}) (.{27}) (.{2}) ?(.*)?$')
 DATE_REGEX = re.compile(r'^(\d+)-(.+)-(\d+)')
 
