6722b864c40b6541f69f2f69fd15a20a1923c890
Andrew Palmer <atp@sdf.org>
2020-05-29 01:12:25 -0500

    Clean up comments

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.css';
@@ -23,7 +21,6 @@ Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = false;
 
-/* eslint-disable no-new */
 new Vue({
   router,
   render: h => h(App),
